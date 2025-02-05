2032181a0982ca157f2c958f49966030bf3790e0
Danielgv010 <danielgarciav010@gmail.com>
2025-02-05 12:49:24 +0100

    Fixed the installer

--- installer.bat
+++ installer.bat
@@ -1,5 +1,6 @@
 py -m venv entorno
-entorno\Scripts\activate
-pip install -r requirements.txt
 git clone https://github.com/Danielgv010/adobe-faq-chatbot
-echo AI_SERVICE_ENDPOINT=""\nAI_SERVICE_KEY=""\nQA_PROJECT_NAME=""\nQA_DEPLOYMENT_NAME="" > qna_bot/.env
+call entorno\Scripts\activate.bat
+pip install -r adobe-faq-chatbot/requirements.txt
+call entorno\Scripts\deactivate.bat
+echo AI_SERVICE_ENDPOINT=""\nAI_SERVICE_KEY=""\nQA_PROJECT_NAME=""\nQA_DEPLOYMENT_NAME="" > qna_bot
