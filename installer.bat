08679060fc8d4dfcd2cb51dca2e305147026c2b1
Danielgv010 <danielgarciav010@gmail.com>
2025-02-06 11:39:16 +0100

    Fixed .env creation

--- installer.bat
+++ installer.bat
@@ -4,6 +4,6 @@ call entorno\Scripts\activate.bat
 pip install -r adobe-faq-chatbot/requirements.txt
 call entorno\Scripts\deactivate.bat
 echo AI_SERVICE_ENDPOINT="" > adobe-faq-chatbot/qna_bot/.env
-echo QA_DEPLOYMENT_NAME="" >> adobe-faq-chatbot/qna_bot/.env
+echo AI_SERVICE_KEY="" >> adobe-faq-chatbot/qna_bot/.env
 echo QA_PROJECT_NAME="" >> adobe-faq-chatbot/qna_bot/.env
 echo QA_DEPLOYMENT_NAME="" >> adobe-faq-chatbot/qna_bot/.env
